62f0c9f56d02af1980b2eb55cc285fb9455a494d
Nivas S <nivas7624@gmail.com>
2025-08-31 09:39:19 +0530

    Initial commit

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,25 @@
+// d:/my-portfolio/frontend/src/hooks/useTheme.js
+import { useEffect, useState } from "react";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") || "light";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return [theme, toggleTheme];
+};
+
+export default useTheme;
